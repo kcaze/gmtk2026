feb96c6d26585b44054db06f582136b926fc7a31
Herman Chau <beijingherman@gmail.com>
2026-07-24 21:57:13 -0700

    Trying to flesh out some game ideas.

--- assets/sprites/export.sh
+++ assets/sprites/export.sh
@@ -3,3 +3,5 @@
 alias ase="/Applications/Aseprite.app/Contents/MacOS/aseprite"
 
 ase -b tracks.aseprite --save-as track-{slice}.png
+ase -b hourglass_indicator.aseprite --save-as hourglass-indicator-{slice}.png
+ase -b soul.aseprite --save-as {slice}.png
