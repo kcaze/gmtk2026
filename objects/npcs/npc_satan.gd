b76113f8275c7daae1d1a9c23be008176f39f7bb
Herman Chau <beijingherman@gmail.com>
2026-07-26 00:55:14 -0700

    Ready for a sample export

---
+++ objects/npcs/npc_satan.gd
@@ -0,0 +1,52 @@
+extends Node2D
+
+var DIALOGUE = [
+	"SATAN:[color=#939090] Oh, it strikes, it strikes![/color]",
+	"[color=#939090]The hour of midnight is near![/color]",
+	"[color=#939090]Hurry, hurry, to the nightmare...[/color]",
+]
+var player_entered = false
+var active = false
+var dialogue_can_play = true
+
+func _ready():
+	$InteractableArea.body_entered.connect(_on_body_entered)
+	$InteractableArea.body_exited.connect(_on_body_exited)
+
+func _on_body_entered(body: Node2D):
+	if body == G.player:
+		player_entered = true
+
+func _on_body_exited(body: Node2D):
+	if body == G.player:
+		player_entered = false
+
+func _process(delta: float) -> void:
+	if not active and is_activated():
+		activate()
+	if active and not is_activated():
+		deactivate()
+	
+	if active and player_entered and dialogue_can_play:
+		print("hi")
+		dialogue_can_play = false
+		G.ui.start_dialogue(DIALOGUE, 3)
+
+func activate():
+	active = true
+	$AnimatedSprite2D.play("active")
+
+
+func deactivate():
+	active = false
+	$AnimatedSprite2D.play("idle")
+
+func is_activated():
+	if G.player.hourglass_active:
+		return $CollisionShape2D.shape.collide(
+			$CollisionShape2D.global_transform,
+			G.player.get_node("HourglassEffect/Collision").shape,
+			G.player.get_node("HourglassEffect/Collision").global_transform
+		)
+	else:
+		return false
--- objects/soul.gd
+++ objects/soul.gd
@@ -2,11 +2,14 @@ extends AnimatedSprite2D
 
 var is_active = true
 var player_colliding = false
-const RESPAWN_TIME = 2
+const RESPAWN_TIME = 0.5
 
 func _process(delta: float) -> void:
 	if player_colliding and is_active:
 		_add_flip()
+	if not is_active and G.player.num_flips == 0 and not G.player.hourglass_active:
+		$RespawnTimer.start(RESPAWN_TIME)
+
 
 func _on_body_entered(body: Node2D) -> void:
 	if body == G.player:
@@ -22,7 +25,6 @@ func _add_flip():
 	G.player.num_flips = 1
 	is_active = false
 	animation = "inactive"
-	$RespawnTimer.start(RESPAWN_TIME)
 
 func _on_respawn_timer_timeout() -> void:
 	is_active = true
